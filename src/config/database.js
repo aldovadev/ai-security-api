95ce06e123eb24e1fcca8b60c2eaacf175a37a3c
yunomi811 <aldova811@gmail.com>
2023-09-24 17:55:11 +0700

    add(.env): edit db

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const db = new Sequelize(
@@ -7,7 +8,7 @@ const db = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST,
+    socketPath: `/cloudsql/${process.env.INSTANCE_NAME}`,
     dialect: "postgres",
   }
 );
