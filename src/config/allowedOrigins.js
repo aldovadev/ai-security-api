38f724d8a0fef92c81ed6808e2cf8ea12d0db3e8
yunomi811 <aldova811@gmail.com>
2023-11-05 15:05:14 +0700

    add(cors): add allowed origin 8080

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['*', 'http://localhost:4200', 'https://ai-security-app-xqsfr2c4aq-et.a.run.app', 'https://ai-security-ml-xqsfr2c4aq-et.a.run.app'];
+const allowedOrigins = ['*', 'http://localhost:8080', 'http://localhost:4200', 'https://ai-security-app-xqsfr2c4aq-et.a.run.app', 'https://ai-security-ml-xqsfr2c4aq-et.a.run.app'];
 
 export default allowedOrigins;
